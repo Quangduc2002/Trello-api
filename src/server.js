3b5ae8b51ec38d6e4308a7122650fce01830bb44
Quangduc2002 <phamquangduc110@gmail.com>
2025-02-05 10:09:13 +0700

    chore: update file server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,3 @@
-// const express = require("express");
-// require("dotenv").config();
-// const { CONNECT_DB, GET_DB } = require("./config/mongodb");
 import express from "express";
 import { CONNECT_DB, GET_DB } from "./config/mongodb.js";
 import "dotenv/config";
@@ -9,29 +6,23 @@ const PORT = process.env.PORT || 8080;
 import { corsOptions } from "./config/cors.js";
 import cors from "cors";
 
-// const START_SERVER = () => {
-const app = express();
-CONNECT_DB();
+const START_SERVER = () => {
+  const app = express();
+  // CONNECT_DB();
 
-app.use(cors(corsOptions));
-app.use(express.json());
-router(app);
+  app.use(cors(corsOptions));
+  app.use(express.json());
+  router(app);
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+};
 
-app.get("/", async (req, res) => {
-  console.log(await GET_DB().listCollections().toArray());
-
-  res.send("<h1>QuangDucDev</h1>");
-});
-// };
-
-// CONNECT_DB()
-//   .then(() => console.log("Connected to database"))
-//   .then(() => START_SERVER())
-//   .catch((err) => {
-//     console.error(err);
-//     process.exit(0);
-//   });
+CONNECT_DB()
+  .then(() => console.log("Connected to database"))
+  .then(() => START_SERVER())
+  .catch((err) => {
+    console.error(err);
+    process.exit(0);
+  });
