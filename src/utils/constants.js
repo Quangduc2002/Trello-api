23571c5c91098fb8bbd340be2507e36ae2d53762
Quangduc2002 <phamquangduc110@gmail.com>
2025-02-18 17:56:30 +0700

    ref: update list domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // "http://localhost:3000"
   "https://trello-web-pqd.vercel.app",
+  "https://trello-our2t3y0q-quangduc2002s-projects.vercel.app",
 ];
 export const BOARD_TYPES = {
   PUBLIC: "public",
